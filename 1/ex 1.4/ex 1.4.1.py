ecf92c27cd1c1fe545aeafe8e3432e99ba6db11a
Danila <shoodanyaters@gmail.com>
2022-10-13 03:39:52 +0300

    Сделал задания ex 1.4

---
+++ 1/ex 1.4/ex 1.4.1.py
@@ -0,0 +1,14 @@
+from numpy import random
+
+n = 100000
+successes = 0
+for _ in range(n):
+    count = 0
+    while True:
+        if random.choice([True, False], p=[0.47, 0.53]) and count < 7:
+            count += 1
+        else:
+            break
+    if count == 7:
+        successes += 1
+print(round(successes/n, 3))
