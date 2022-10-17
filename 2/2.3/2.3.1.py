846010d575adc29ed2a03e2de72a335fd1a2c0f8
Danila <shoodanyaters@gmail.com>
2022-10-17 18:49:50 +0300

    сделал задание 2.3 раздела

--- 2/2.3/2.3.1.py
+++ 2/2.3/2.3.1.py
@@ -4,4 +4,5 @@ from numpy import median
 values_xi = [-4, -2, 0, 3, 4, 6, 7, 9]
 probabilities_xi = [0.1, 0.15, 0.05, 0.15, 0.1, 0.05, 0.25, 0.15]
 pool = choice(values_xi, 10000000, p=probabilities_xi)
-print(f'expected value = {round(pool.mean(), 3)}, median = {median(pool)}')
+print(f'expected value = {round(pool.mean(), 3)},'
+      f' median = {median(pool)}')
