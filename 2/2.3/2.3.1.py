bd255c4856a3e9ea1e5bb58a2a3d16b3c9a20f9a
Danila <shoodanyaters@gmail.com>
2022-10-17 18:23:02 +0300

    сделал заданиt 2.3 раздела

---
+++ 2/2.3/2.3.1.py
@@ -0,0 +1,7 @@
+from numpy.random import choice
+from numpy import median
+
+values_xi = [-4, -2, 0, 3, 4, 6, 7, 9]
+probabilities_xi = [0.1, 0.15, 0.05, 0.15, 0.1, 0.05, 0.25, 0.15]
+pool = choice(values_xi, 10000000, p=probabilities_xi)
+print(f'expected value = {round(pool.mean(), 3)}, median = {median(pool)}')
